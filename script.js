06f3e63456c2ec6c9ea5599bcf6a474938bcd92a
NMy <5076329+NcMhlr@users.noreply.github.com>
2021-12-10 10:52:38 +0100

    Object Methoden funktionieren jetzt

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 
 const zitatObjekt = {
     arrKategorie: ['Leben','Arbeit','Familie'],
-    arrAutor: ['Steve Jobs'],
+    arrAutor: ['Steve Jobs','Barak Obama'],
     arrZitat: [['Vergeuden Sie nicht Ihre Zeit damit, das Leben eines anderen zu leben.','Steve Jobs','Leben'],['','','']],
 
-zufallAusArray: (arr) => {
+zufallAusArray(arr) {
     const zufallsZahl = Math.floor(Math.random() * arr.length);
     return arr[zufallsZahl];
 },
 
-zufallZitat: () => {
+filterArray(arr, source) {
+
+},
+
+zufallZitat() {
     const zufallsZahl = Math.floor(Math.random() * 3) + 1;
     switch (zufallsZahl) {
         case 1:
-            console.log(this.arrKategorie);
-            return 'Ein zufälliges Zitat aus einer einer zufälligen Kategorie.';
+            return {
+            description: 'Ein zufälliges Zitat aus einer einer zufälligen Kategorie.',
+            categorie: this.zufallAusArray(this.arrKategorie),
+            autor: '',
+            zitat: ''
+            };
         break;
         case 2:
-            return 'Ein zufälliges Zitat eines zufälligen Autors.';
+            return {
+            description: 'Ein zufälliges Zitat eines zufälligen Autors.',
+            categorie: 'noch nicht verfügbar',
+            autor: this.zufallAusArray(this.arrAutor),
+            zitat: ''
+        };
         break;
         case 3:
             return 'Ein zufälliges Zitat.';
         break;
     }
 }
-
-
-
 };
 
-
-
-
-
 for(i=0;i < 5; i++){
-console.log(zitatObjekt.zufallZitat());
-zitatObjekt.zufallAusArray(zitatObjekt.arrKategorie)
+const zitat = zitatObjekt.zufallZitat();
+console.log(zitat.description + ' ' + zitat.categorie + ' ' + zitat.autor);
+//console.log(zitatObjekt.zufallAusArray(zitatObjekt.arrKategorie));
 };
