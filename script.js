51653fd72e422c9558d707117251532eca9bde4d
NMy <5076329+NcMhlr@users.noreply.github.com>
2021-12-12 21:49:26 +0100

    großer Umbau erfolgreich, Code etwas schlanker

--- script.js
+++ script.js
@@ -1,63 +1,61 @@
 const zitatObjekt = {
-    arrKategorie: ['Leben', 'Zukunft'],
-    arrAutor: ['Steve Jobs', 'Barak Obama'],
-    arrZitat: [['Vergeuden Sie nicht Ihre Zeit damit, das Leben eines anderen zu leben.', 'Steve Jobs', 'Leben'],
-        ['Dies ist nicht der Beginn vom Ende der Krise, sondern das Ende vom Beginn.', 'Barak Obama', 'Zukunft']],
+    arrZitat: [
+        ['Vergeuden Sie nicht Ihre Zeit damit, das Leben eines anderen zu leben.', 'Steve Jobs', 'Leben'],
+        ['Dies ist nicht der Beginn vom Ende der Krise, sondern das Ende vom Beginn.', 'Barak Obama', 'Zukunft'],
+        ['Nur tote Fische schwimmen mit dem Strom.', 'Indianer', 'Opportunismus'],
+        ['Filmemacher sollten bedenken, dass man ihnen am Tag des Jüngsten Gerichts all ihre Filme wieder vorspielen wird.','Charlie Chaplin','Film'],
+        ['Wer ständig glücklich sein möchte, muss sich oft verändern.','Konfuzius','Leben']
+    ],
 
-    zufallAusArray(arr) {
-        const zufallsZahl = Math.floor(Math.random() * arr.length);
-        //console.log(arr[zufallsZahl]);
-        return arr[zufallsZahl];
+    // Random Type
+    // 0 Random Quote from arrZitat
+    // 1 Random Quote from Autor
+    // 2 Random Quote from Category 
+    randomQuoteBy() {
+        const rndNumber = Math.floor(Math.random() * 3);
+        return rndNumber;
     },
 
+    // Random Element from an given Array
+    randomElementFromArray(arr) {
+        const rndNumber = Math.floor(Math.random() * arr.length);
+        return arr[rndNumber];
+    },
+
+    // Filter method wich filters the Array by a given category or autor
+    // if the type is 0 -> returned complete Array 
     filterArray(arr, source, type) {
-        return filteredArray = arr.filter(e => {return e[type] === source});
-
+            return filteredArray = arr.filter(e => {
+                    return e[type] === source
+                });
     },
 
-    zufallZitat() {
-        const zufallsZahl = Math.floor(Math.random() * 3) + 1;
-        switch (zufallsZahl) {
-            case 1:
-                const tempArr = this.filterArray(this.arrZitat, this.zufallAusArray(this.arrKategorie), 2);
-                // console.log(tempArr1)
-                const obj = this.zufallAusArray(tempArr);
-
-                return {
-                    description: 'Ein zufälliges Zitat aus einer einer zufälligen Kategorie.',
-                    categorie: obj[2],
-                    autor: obj[1],
-                    zitat: obj[0],
-                };
-                break;
-            case 2:
-                const tempArrB = this.filterArray(this.arrZitat, this.zufallAusArray(this.arrAutor), 1);
-                // console.log(tempArrB)
-                const obj_B = this.zufallAusArray(tempArrB);
-
-                return {
-                    description: 'Ein zufälliges Zitat eines zufälligen Autors.',
-                        categorie: obj_B[2],
-                        autor: obj_B[1],
-                        zitat: obj_B[0]
-                };
-                break;
-            case 3:
-                const obj2 = this.zufallAusArray(this.arrZitat)
-                return {
-                    description: 'Ein zufälliges Zitat.',
-                        categorie: obj2[2],
-                        autor: obj2[1],
-                        zitat: obj2[0],
-                }
-                break;
+    createTempArray(arr, type) {
+        const tempArray = [];
+            for(let e of arr){
+                tempArray.push(e[type]);
+            };
+        return tempArray;
+        },
+    
+    printMessage() {
+        const randomNumber = this.randomQuoteBy();
+        console.log(randomNumber);
+        if(randomNumber < 1){
+                // console.log(randomNumber);
+                console.log(this.randomElementFromArray(this.arrZitat));
+             }else{
+                 //console.log(randomNumber);
+                 console.log(this.createTempArray(this.arrZitat, randomNumber));
+                 console.log(this.randomElementFromArray(this.filterArray(this.arrZitat, this.randomElementFromArray(this.createTempArray(this.arrZitat, randomNumber)), randomNumber)));
         }
-    }
+    },
 };
 
-for (i = 0; i < 1; i++) {
-    const zitat = zitatObjekt.zufallZitat();
-    console.log(zitat.description + '\nKategrie: ' + zitat.categorie + '\nAutor: ' + zitat.autor + '\nZitat: ' + zitat.zitat);
-    console.log('--------------------------------------------------------------------------------');
+
+for (i = 0; i < 10; i++) {
+    zitatObjekt.printMessage();
+    //console.log(zitatObjekt.createTempArray(zitatObjekt.arrZitat, 2));
+    
     //console.log(zitatObjekt.zufallAusArray(zitatObjekt.arrKategorie));
 };
